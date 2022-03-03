c2965e4e3906d37b9c6df2aab123b127974ac38a
Dan Stanhope <danstanhope@dans-mbp-2.lan>
2022-03-03 10:34:28 -0500

    changes

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,13 +1,3 @@
 export const formatMoney = (value: number) => {   
-    let val:string;
-
-    if (value >= 1000000) {
-        val = `$${Intl.NumberFormat().format(value / 1000000)}M`;
-    } else if (value >= 1000) {
-        val = value % 1000 === 0 ? `$${Intl.NumberFormat().format(value/1000)}K` : `$${Intl.NumberFormat().format(value)}`;    
-    } else {
-        val = `$${Intl.NumberFormat().format(value)}`;
-    }
-
-    return val;
+    return `$${Intl.NumberFormat().format(value)}`;
 };
